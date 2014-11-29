37658aa64e35517fa3330f9a5a35dcb27a9ea0f0
Sam Georges <sam@daftspunk.com>
2014-11-29 20:53:58 +1100

    Create an AJAX modal script, implement on browse page

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+/*
+ * Application
+ *
+ */
+
+/*
+ * Dry UI
+ *
+=require ../vendor/dry-ui/js/ajax-modal
+ */
+
+/*
+ * Vendor
+ *
+ */
+
+$(document).render(function(){
+
+    $('[data-control=dropdown]').dropdown()
+
+})
---
+++ assets/javascript/pages/browse.js
@@ -0,0 +1,19 @@
+var styles = [{"stylers":[{"saturation":-100}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#0099dd"}]},{"elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#aadd55"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"labels.text","stylers":[{"visibility":"on"}]},{"featureType":"road.local","elementType":"labels.text","stylers":[{"visibility":"on"}]},{}]
+
+new GMaps({
+    div: '#map',
+    lat: -12.043333,
+    lng: -77.028333,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    mapTypeControl: false,
+    scaleControl: false,
+    zoomControl: true,
+    streetViewControl: false,
+    zoomControlOptions: {
+        style: google.maps.ZoomControlStyle.SMALL
+    },
+    mapTypeControlOptions: {
+        mapTypeIds: [google.maps.MapTypeId.ROADMAP]
+    }, 
+    styles: styles
+});
